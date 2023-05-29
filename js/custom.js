07e7f8b13fc0199284d6890515f7a430c9b4e057
Y Kassabi <22109127+ykassabi@users.noreply.github.com>
2023-05-29 22:45:44 +0200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+
+var pAgree = '1';
+window.addEventListener('load', function() {
+	$('a.smooth').smoothScroll({speed: 800});
+});
+window.addEventListener('load', function() {
+		var rellax = new Rellax('.parallax', {
+			center: true
+		});
+});
